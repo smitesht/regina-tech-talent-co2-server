abce457ea44049dfb863f75db90113bb51f00dbb
Smitesh Tamboli <52151346+smitesht@users.noreply.github.com>
2023-11-08 23:13:33 -0600

    Delete cars.js

--- cars.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const {  getCarsMake,getCarsModel } = require('../controllers/cars');
-
-const router = express.Router();
-
-router.route('/make').get(getCarsMake)
-router.route('/model/:id').get(getCarsModel)
-module.exports = router;
